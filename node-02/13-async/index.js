8425f262ec3585999c743a1b6816c0e572cfdbb5
Dari Alves <darieltondf@gmail.com>
2020-10-31 20:27:56 -0300

    13-async

--- node-02/13-async/index.js
+++ node-02/13-async/index.js
@@ -3,7 +3,8 @@ const fs = require('fs').promises
 printLengths('./');
 
 async function printLengths (dir) {
-    const fileList = await fs.readdir(dir);
+    const fileList = await fs.readdir(dir)
+
     const results = fileList.map(async file => await fs.readFile(file)
         .then(data => [file, data.length]))
 
